dc1ec21bd9c9a1e85f026c29d8934a13b83181db
Andy Chen <andy200511@126.com>
2019-07-13 12:19:25 -0400

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,36 @@
+<script>
+  let maxValue = 1;
+  let minValue = 0;
+  import RandomNum from "./RandomNum";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+  #maxValue {
+    height: 2em;
+  }
+  #minValue {
+    height: 2em;
+  }
+</style>
+
+<main>
+  <h1>Random Number Playground</h1>
+</main>
+
+<label for="minValue">Min Random Value</label>
+<input type=number bind:value={minValue} id="minValue">
+
+<br>
+<br>
+
+<label for="maxValue">Max Random Value:</label>
+<input type=number bind:value={maxValue} id="maxValue" />
+
+<br>
+<br>
+
+<RandomNum maxValue={maxValue} minValue={minValue} />
---
+++ RandomNum.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let maxValue;
+  export let minValue;
+  let result = 0;
+
+  function genRand() {
+    result = Math.random() * (maxValue - minValue) + minValue;
+  }
+
+  function genRandInt() {
+    const min = Math.ceil(minValue);
+    const max = Math.floor(maxValue);
+    result = Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function reset() {
+    result = 0;
+  }
+</script>
+
+<style>
+  button {
+    padding: 1em 2em
+  }
+</style>
+
+<p>{result}</p>
+
+<button on:click={genRand}>Generate Random Number</button>
+<button on:click={genRandInt}>Generate Random Integer</button>
+<button on:click={reset}>Reset</button>
